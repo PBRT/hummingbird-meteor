37d3bcc59beb428427ff2b9ac5d5eea710d9c2a1
Theodor Vararu <theo@vararu.org>
2015-10-24 23:22:32 +0100

    Alignment.

--- react/App.jsx
+++ react/App.jsx
@@ -1,10 +1,13 @@
-import { Component, PropTypes } from 'react'
+import { Component } from 'react'
+import PropTypes from 'lib/propTypes'
 import reactMixin from 'react-mixin'
 import Header from 'components/header'
 import Footer from 'components/footer'
 
 @reactMixin.decorate(ReactMeteorData)
 export default class App extends Component {
+  static displayName = 'App'
+
   static propTypes = {
     children: PropTypes.node.isRequired
   }
@@ -21,7 +24,7 @@ export default class App extends Component {
   }
 
   render () {
-    return <div style={{ paddingTop: '60px' }}>
+    return <div style={{ height: '100%', paddingTop: '60px' }}>
       <Header user={ this.data.user } />
       { this.props.children }
       <Footer />
--- react/index.jsx
+++ react/index.jsx
@@ -8,14 +8,35 @@ import Trips from 'handlers/Trips'
 import Trip from 'handlers/Trip'
 import NewTrip from 'handlers/NewTrip'
 
+import PropTypes from 'lib/propTypes'
+import { Grid, Row, Col } from 'react-bootstrap'
+
+class Wrapper extends require('react').Component {
+  static displayName = 'Wrapper'
+
+  static propTypes = {
+    children: PropTypes.node.isRequired
+  }
+
+  render () {
+    return <Grid style={{ paddingTop: '20px', paddingBottom: '20px', minHeight: 'calc(100% - 80px)' }}>
+      <Row>
+        <Col xs={ 12 }>{ this.props.children }</Col>
+      </Row>
+    </Grid>
+  }
+}
+
 ReactRouterSSR.Run(
   <Route path='/' component={ App }>
     <IndexRoute component={ Landing } />
-    <Route path='users/:userId' component={ User } />
-    <Route path='trips'>
-      <IndexRoute component={ Trips } />
-      <Route path='new' component={ NewTrip } />
-      <Route path=':tripId' component={ Trip } />
+    <Route component={ Wrapper }>
+      <Route path='users/:userId' component={ User } />
+      <Route path='trips'>
+        <IndexRoute component={ Trips } />
+        <Route path='new' component={ NewTrip } />
+        <Route path=':tripId' component={ Trip } />
+      </Route>
     </Route>
   </Route>
 )
