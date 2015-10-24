e1bb199c2bd2d2b86f6f6278aea98ecf179269bb
Theodor Vararu <theo@vararu.org>
2015-10-24 16:15:04 +0100

    Adding Facebook login.

--- react/components/Test.jsx
+++ react/components/Test.jsx
@@ -1,11 +1,14 @@
 import { Component } from 'react'
 import { Button } from 'react-bootstrap'
 
+const LoginButtons = BlazeToReact('loginButtons')
+
 export default class Test extends Component {
   static displayName = 'Test'
 
   render () {
     return <div>
+      <LoginButtons />
       <Button>Hello world</Button>
       Hello world
     </div>
