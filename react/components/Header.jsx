dcffed688b8a8cbed3f14d31731c549232428ed9
Theodor Vararu <theo@vararu.org>
2015-10-24 19:52:56 +0100

    Fix links in Header.jsx.

--- react/components/Header.jsx
+++ react/components/Header.jsx
@@ -1,8 +1,11 @@
 import { Component } from 'react'
 import PropTypes from 'lib/propTypes'
 import { Navbar, NavBrand, Nav, NavItem, Modal } from 'react-bootstrap'
+import reactMixin from 'react-mixin'
+import { History } from 'react-router'
 const LoginButtons = BlazeToReact('loginButtons')
 
+@reactMixin.decorate(History)
 export default class Header extends Component {
   static displayName = 'Header'
 
@@ -16,17 +19,27 @@ export default class Header extends Component {
       isModalVisible: false
     }
     this.handleModal = this.handleModal.bind(this)
+    this.goToHomepage = this.goToHomepage.bind(this)
+    this.goToProfile = this.goToProfile.bind(this)
   }
 
   handleModal (val) {
     this.setState({ isModalVisible: val })
   }
 
+  goToHomepage () {
+    this.history.pushState(null, `/`)
+  }
+
+  goToProfile () {
+    this.history.pushState(null, `/user/${this.props.user._id}`)
+  }
+
   renderLogin () {
     const user = this.props.user
     if (user) {
       return <Nav navbar right>
-        <NavItem href={ `/user/${user._id}` }>{user.profile.name}</NavItem>
+        <NavItem onClick={this.goToProfile}>{user.profile.name}</NavItem>
         <NavItem onClick={this.handleModal.bind(null, true)}>LOGOUT</NavItem>
       </Nav>
     } else {
@@ -39,7 +52,7 @@ export default class Header extends Component {
   render () {
     return (
       <Navbar fixedTop style={{marginBottom: 0}}>
-        <NavBrand>HummingBird</NavBrand>
+        <NavBrand style={{ cursor: 'pointer' }} onClick={this.goToHomepage}>HummingBird</NavBrand>
         { this.renderLogin() }
         <Modal
           show={this.state.isModalVisible}
