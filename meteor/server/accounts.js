c23c49fbebd263b962ce7e1282f3b4a5ebf7801f
Theodor Vararu <theo@vararu.org>
2015-10-25 15:59:36 +0000

    Add signup email.

--- meteor/server/accounts.js
+++ meteor/server/accounts.js
@@ -1,3 +1,5 @@
+/* global Email */
+
 Accounts.onCreateUser(function (options, user) {
   if (options.profile) {
     user.profile = options.profile
@@ -7,6 +9,15 @@ Accounts.onCreateUser(function (options, user) {
     user.profile.firstName = user.services.facebook.first_name
     user.profile.email = user.services.facebook.email
     user.profile.link = user.services.facebook.link
+  }
+
+  if (user.profile.email) {
+    Email.send({
+      from: 'hummingbird@birds.vararu.org',
+      to: user.profile.email,
+      subject: 'Welcome to Hummingbird!',
+      text: 'O hai there!\n\nYou\'ve just signed up for Hummingbird. Yay!'
+    })
   }
 
   return user
